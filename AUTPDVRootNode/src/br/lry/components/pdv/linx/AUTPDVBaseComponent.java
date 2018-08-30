5116a96231579513df4441189c97c373ce77959d
marcosoliveirastk <marcos.oliveira@softtek.com>
2018-08-30 00:55:19 -0300

    Inclusão de funções básicas - PDV

--- AUTPDVRootNode/src/br/lry/components/pdv/linx/AUTPDVBaseComponent.java
+++ AUTPDVRootNode/src/br/lry/components/pdv/linx/AUTPDVBaseComponent.java
@@ -15,6 +15,32 @@ public class AUTPDVBaseComponent {
 	com.borland.silktest.jtf.Desktop AUT_AGENT_SILK4J = new com.borland.silktest.jtf.Desktop();
 	com.borland.silktest.jtf.BaseState AUT_AGENT_SILK4J_CONFIGURATION = new com.borland.silktest.jtf.BaseState();
 	
+	public interface AUTPDVFunctionsSyncronized{
+		public boolean autStartPDVFunction();
+	}
+	
+	
+	public enum AUT_PDV_OPTIONS{
+		ENTER,
+		ENTRADA_OPERADOR,
+		TEMPO_ENTRE_INTERACOES_LOOPS;
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			switch(this) {
+			case ENTER:{
+				return "<#Enter>";
+				}
+			case ENTRADA_OPERADOR:{
+				return "1";
+			}
+			case TEMPO_ENTRE_INTERACOES_LOOPS:{
+				return "6000";
+			}
+			}
+			return super.toString();
+		}
+	}
 	
 	/**
 	 * Inicia a aplicação pdv
@@ -42,13 +68,15 @@ public class AUTPDVBaseComponent {
 	 * @return boolean - True o PDV esteja fechado parcialmente e disponível, false caso contrário
 	 * 
 	 */
-	public boolean autPDVFechadoParcial() {
+	public boolean autPDVStatusFechadoParcial() {
 		try {
-						
+			
+			
 			AUT_AGENT_SILK4J.verifyAsset("PDV-STATUS-0001");
 			
-			System.out.println("PDV: AUT INFO : VERIFICANDO STATUS : FECHADO PARCIAL");
+			System.out.println("PDV: AUT INFO : STATUS ATUAL : CAIXA FECHADO PARCIAL");
 
+			
 			return true;
 		}
 		catch(java.lang.Exception e) {
@@ -59,6 +87,154 @@ public class AUTPDVBaseComponent {
 			return false;
 		}
 	}
+	
+	public <TPDVFunction extends AUTPDVFunctionsSyncronized> void autPDVExecFuncSincronizada(TPDVFunction functionPDV) {
+		try {
+			
+			boolean bOk = false;
+			bOk = functionPDV.autStartPDVFunction();
+			while(!bOk) {
+				bOk = functionPDV.autStartPDVFunction();
+				com.borland.silktest.jtf.Utils.sleep(3000);
+			}
+		}
+		catch(com.borland.silktest.jtf.common.VerificationFailedException e) {
+			
+		}
+	}
+	
+	
+	
+	/**
+	 * Envia dados de entrada para PDV
+	 * 
+	 * @param dados - Dados de entrada do caixa
+	 */
+	public void autPDVEntradaDados(Object dados) {
+		String strFrt = "<#%s>";		
+		for(java.lang.Character c : dados.toString().toCharArray()) {
+			AUT_AGENT_SILK4J.<com.borland.silktest.jtf.Control>find("PDV").typeKeys(String.format(strFrt, c));			
+		}
+	}
+
+	/**
+	 * 
+	 * Envia dados para PDV
+	 * 
+	 * @param dados - Dados de entrada para PDV
+	 * @param tempoDelayEntreTeclas - Tempo de espera entre cada caractere da string de dados
+	 * 
+	 */
+	public void autPDVEntradaDados(Object dados,Integer tempoDelayEntreTeclas) {
+		String strFrt = "<#%s>";		
+		for(java.lang.Character c : dados.toString().toCharArray()) {
+			AUT_AGENT_SILK4J.<com.borland.silktest.jtf.Control>find("PDV").typeKeys(String.format(strFrt, c),tempoDelayEntreTeclas * 1000);
+		}		
+	}
+	
+	/**
+	 * Envia comandos pré configurados para PDV
+	 * 
+	 * @param comandoPDV - Comando definido previamente
+	 */
+	public <TComando extends java.lang.Enum<AUT_PDV_OPTIONS>> void autPDVEnviarComando(TComando comandoPDV) {
+		AUT_AGENT_SILK4J.<com.borland.silktest.jtf.Control>find("PDV").typeKeys(comandoPDV.toString());
+	}
+	
+	/**
+	 * Verifica se o caixa está aguardando o código de entrada do operador 
+	 * 
+	 * @return boolean - True caso o caixa esteja exibindo a mensagem solicitando a entrada do operador
+	 * 
+	 */
+	public boolean autPDVStatusEntradaOperador() {
+		try {
+			
+			AUT_AGENT_SILK4J.verifyAsset("PDV-STATUS-0002");
+			
+			System.out.println("PDV : AUT INFO: VALIDACAO STATUS  ENTRADA OPERADOR: OK");
+			
+			return true;
+		}
+		catch(java.lang.Exception e) {
+			System.out.println("PDV: AUT ERROR: VALIDACAO DE STATUS ENTRADA OPERADOR");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Verifica se o caixa está aguardando senha do operador 
+	 * 
+	 * @return boolean - True caso o caixa esteja exibindo a mensagem solicitando a senha do operador
+	 * 
+	 */
+	public boolean autPDVStatusSenhaOperador() {
+		try {
+			
+			AUT_AGENT_SILK4J.verifyAsset("PDV-STATUS-0003");
+			
+			System.out.println("PDV : AUT INFO: VALIDACAO STATUS  SENHA OPERADOR: OK");
+			
+			return true;
+		}
+		catch(java.lang.Exception e) {
+			System.out.println("PDV: AUT ERROR: VALIDACAO DE STATUS SENHA OPERADOR");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Verifica se o caixa está aguardando um valor para o fundo de troco 
+	 * 
+	 * @return boolean - True caso o caixa esteja exibindo a mensagem solicitando um fundo de troco
+	 * 
+	 */
+	public boolean autPDVStatusFundoDeTroco() {
+		try {
+			
+			AUT_AGENT_SILK4J.verifyAsset("PDV-STATUS-0004");
+			
+			System.out.println("PDV : AUT INFO: VALIDACAO STATUS AG.FUNDO DE TROCO: OK");
+			
+			return true;
+		}
+		catch(java.lang.Exception e) {
+			System.out.println("PDV: AUT ERROR: VALIDACAO DE STATUS AG.FUNDO DE TROCO");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	
+	
+	/**
+	 * Verifica se o caixa está exindo a mensagem caixa disponível
+	 * 
+	 * @return boolean - True caso o caixa esteja exibindo a mensagem caixa disponível
+	 * 
+	 */
+	public boolean autPDVStatusCaixaDisponível() {
+		try {
+			
+			AUT_AGENT_SILK4J.verifyAsset("PDV-STATUS-0005");
+			
+			System.out.println("PDV : AUT INFO: VALIDACAO STATUS CAIXA DISPONIVEL: OK");
+			
+			return true;
+		}
+		catch(java.lang.Exception e) {
+			System.out.println("PDV: AUT ERROR: VALIDACAO DE STATUS CAIXA DISPONIVEL");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
 	public AUTPDVBaseComponent() {
 		
 	}
